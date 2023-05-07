eeeb652acee1a8e6353d5199127415da0dcf0969
mjanez <96422458+mjanez@users.noreply.github.com>
2023-05-07 13:03:20 +0200

    Add ckanext-facet_scheming
    facet_scheming is designed to provide templates and functions to be used by other extensions over it. It uses the fields defined in a scheming file to provide a set of tools to use those fields for scheming, and a way to include icons in its labels when displaying them.
    
    - Fix 02_setup_scheming to insert ckanext-facet_scheming ennvars and solr rebuild index.
    - Update Dockerfile.spatial
    - Update .env.example

--- ckan/docker-entrypoint.d/02_setup_scheming.sh
+++ ckan/docker-entrypoint.d/02_setup_scheming.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# Update ckanext-scheming settings defined in the env var
-echo "Loading ckanext-scheming settings into ckan.ini"
+# Update ckanext-scheming and ckanext-facet_scheming settings defined in the env var
+echo "Set up ckanext-facet_scheming. Clear index"
+ckan -c $CKAN_INI search-index clear
+
+echo "Loading ckanext-scheming and ckanext-facet_scheming settings into ckan.ini"
 ckan config-tool $CKAN_INI \
     "scheming.dataset_schemas=$SCHEMA_CKANEXT_SCHEMING_DATASET_SCHEMA" \
     "scheming.group_schemas=$SCHEMA_CKANEXT_SCHEMING_GROUP_SCHEMAS" \
     "scheming.organization_schemas=$SCHEMA_CKANEXT_SCHEMING_ORGANIZATION_SCHEMAS" \
-    "scheming.presets=$SCHEMA_CKANEXT_SCHEMING_PRESETS"
+    "scheming.presets=$SCHEMA_CKANEXT_SCHEMING_PRESETS" \
+    "facet_scheming.facet_list=$CKANEXT__FACET_FACET_LIST"
+
+echo "ckanext-facet_scheming. Rebuild index"
+ckan -c $CKAN_INI search-index rebuild
