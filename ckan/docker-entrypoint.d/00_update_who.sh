eba3896b44cd2b632a9879ebf8e33bd6966c54a6
mjanez <96422458+mjanez@users.noreply.github.com>
2023-07-26 10:25:03 +0200

    Fix nginx multiple locations
    - Replace httpd with nginx default docker-compose file
    - Update nginx config
    - Update httpd config
    - Fix Dockerfiles

--- ckan/docker-entrypoint.d/00_update_who.sh
+++ ckan/docker-entrypoint.d/00_update_who.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Update who.ini when exists APACHE_CKAN_LOCATION
+# Update who.ini when exists PROXY_CKAN_LOCATION
 echo "Update who.ini"
-if [ -n "$APACHE_CKAN_LOCATION" ] && [ "$APACHE_CKAN_LOCATION" != "/" ]; then \
-        sed -i "s|\${WHO_LOCATION}|$APACHE_CKAN_LOCATION|g" ${APP_DIR}/who.ini; \
+if [ -n "$PROXY_CKAN_LOCATION" ] && [ "$PROXY_CKAN_LOCATION" != "/" ]; then \
+        sed -i "s|\${WHO_LOCATION}|$PROXY_CKAN_LOCATION|g" ${APP_DIR}/who.ini; \
     else \
         sed -i "s|\${WHO_LOCATION}|/|g" ${APP_DIR}/who.ini; \
     fi
