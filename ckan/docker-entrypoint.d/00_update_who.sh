a2fa91798b7b812a1ead9928b70d7c43da5dc7c3
mjanez <96422458+mjanez@users.noreply.github.com>
2023-07-26 12:02:40 +0200

    Fix who.ini when is only: "/"
    Avoid replace to "//" in 00_update_who.sh entrypoint

--- ckan/docker-entrypoint.d/00_update_who.sh
+++ ckan/docker-entrypoint.d/00_update_who.sh
@@ -2,8 +2,14 @@
 
 # Update who.ini when exists PROXY_CKAN_LOCATION
 echo "Update who.ini"
-if [ -n "$PROXY_CKAN_LOCATION" ] && [ "$PROXY_CKAN_LOCATION" != "/" ]; then \
-        sed -i "s|\${WHO_LOCATION}|$PROXY_CKAN_LOCATION|g" ${APP_DIR}/who.ini; \
-    else \
-        sed -i "s|\${WHO_LOCATION}|/|g" ${APP_DIR}/who.ini; \
+if [ -n "$PROXY_CKAN_LOCATION" ] && [ "$PROXY_CKAN_LOCATION" != "/" ]; then
+    sed -i "s|\${WHO_LOCATION}|$PROXY_CKAN_LOCATION|g" "${APP_DIR}/who.ini";
+else
+    # Check if the value is exactly "/"
+    if [ "$PROXY_CKAN_LOCATION" = "/" ]; then
+        sed -i "s|\${WHO_LOCATION}||g" "${APP_DIR}/who.ini";
+    else
+        # Handle the case when $PROXY_CKAN_LOCATION is empty or not set
+        sed -i "s|\${WHO_LOCATION}|/|g" "${APP_DIR}/who.ini";
     fi
+fi
