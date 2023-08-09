554e2dfbcf33293500c95a920939bb29bedfb00e
mjanez <96422458+mjanez@users.noreply.github.com>
2023-08-09 10:51:04 +0200

    Update plugins - Use ckan/ckanext-scheming vanilla (3.0.0) instead of custom ckanext-scheming - Update all custom templates, tools and schemas to mjanez/ckanext-scheming_dcat

--- ckan/docker-entrypoint.d/02_setup_scheming.sh
+++ ckan/docker-entrypoint.d/02_setup_scheming.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# Update ckanext-scheming and ckanext-facet_scheming settings defined in the env var
-echo "Set up ckanext-facet_scheming. Clear index"
+# Update ckanext-scheming and ckanext-scheming_dcat settings defined in the env var
+echo "Set up ckanext-scheming_dcat. Clear index"
 ckan -c $CKAN_INI search-index clear
 
-echo "Loading ckanext-scheming and ckanext-facet_scheming settings into ckan.ini"
+echo "Loading ckanext-scheming and ckanext-scheming_dcat settings into ckan.ini"
 ckan config-tool $CKAN_INI \
-    "scheming.dataset_schemas=$SCHEMA_CKANEXT_SCHEMING_DATASET_SCHEMA" \
-    "scheming.group_schemas=$SCHEMA_CKANEXT_SCHEMING_GROUP_SCHEMAS" \
-    "scheming.organization_schemas=$SCHEMA_CKANEXT_SCHEMING_ORGANIZATION_SCHEMAS" \
-    "scheming.presets=$SCHEMA_CKANEXT_SCHEMING_PRESETS" \
-    "facet_scheming.facet_list=$CKANEXT__FACET_FACET_LIST"
+    "scheming.dataset_schemas=$CKANEXT__SCHEMING_DCAT_DATASET_SCHEMA" \
+    "scheming.group_schemas=$CKANEXT__SCHEMING_DCAT_GROUP_SCHEMAS" \
+    "scheming.organization_schemas=$CKANEXT__SCHEMING_DCAT_ORGANIZATION_SCHEMAS" \
+    "scheming.presets=$CKANEXT__SCHEMING_DCAT_PRESETS" \
+    "scheming_dcat.facet_list=$CKANEXT__SCHEMING_DCAT_FACET_LIST" \
+    "scheming_dcat.organization_custom_facets=$CKANEXT__SCHEMING_DCAT_ORGANIZATION_CUSTOM_FACETS" \
+    "scheming_dcat.group_custom_facets=$CKANEXT__SCHEMING_DCAT_GROUP_CUSTOM_FACETS"
 
-echo "ckanext-facet_scheming. Rebuild index"
+echo "ckanext-scheming_dcat. Rebuild index"
 ckan -c $CKAN_INI search-index rebuild
