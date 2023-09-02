8392b0a72fb64f900dbf52fbd8e4ef2b6f4bc9a8
mjanez <96422458+mjanez@users.noreply.github.com>
2023-09-02 11:06:40 +0200

    Add geometadata_base_uri ennvar
    - Update 02_setup_scheming.sh
    - Update .env files

--- ckan/docker-entrypoint.d/02_setup_scheming.sh
+++ ckan/docker-entrypoint.d/02_setup_scheming.sh
@@ -12,7 +12,8 @@ ckan config-tool $CKAN_INI \
     "scheming.presets=$CKANEXT__SCHEMING_DCAT_PRESETS" \
     "scheming_dcat.facet_list=$CKANEXT__SCHEMING_DCAT_FACET_LIST" \
     "scheming_dcat.organization_custom_facets=$CKANEXT__SCHEMING_DCAT_ORGANIZATION_CUSTOM_FACETS" \
-    "scheming_dcat.group_custom_facets=$CKANEXT__SCHEMING_DCAT_GROUP_CUSTOM_FACETS"
+    "scheming_dcat.group_custom_facets=$CKANEXT__SCHEMING_DCAT_GROUP_CUSTOM_FACETS" \
+    "scheming_dcat.geometadata_base_uri=$CKANEXT__SCHEMING_DCAT_GEOMETADATA_BASE_URI"
 
 echo "ckanext-scheming_dcat. Rebuild index"
 ckan -c $CKAN_INI search-index rebuild
