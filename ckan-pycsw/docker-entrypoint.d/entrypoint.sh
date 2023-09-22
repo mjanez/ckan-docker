f578862769023f8d1a59a826313e46c95b77cfb0
mjanez <96422458+mjanez@users.noreply.github.com>
2023-09-22 09:02:00 +0000

    Fix ckan-pycsw entrypoint

--- ckan-pycsw/docker-entrypoint.d/entrypoint.sh
+++ ckan-pycsw/docker-entrypoint.d/entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -xeuo pipefail
 
+envsubst < pycsw.conf.template > pycsw.conf
+
 #TODO: -Xfrozen_modules=off from: https://bugs.python.org/issue1666807
 /wait-for --timeout "$TIMEOUT" "$CKAN_URL" -- pdm run python3 -Xfrozen_modules=off ckan2pycsw/ckan2pycsw.py
 
