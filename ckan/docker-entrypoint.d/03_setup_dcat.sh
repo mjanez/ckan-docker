e5447ef32b2bd6a9ce420c74ba357f6ca213b6b4
mjanez <96422458+mjanez@users.noreply.github.com>
2023-09-25 10:00:39 +0200

    Add CKANEXT__DCAT__DEFAULT_CATALOG_ENDPOINT ennvar
    Allow to set the default catalog endpoint of the portal.

--- ckan/docker-entrypoint.d/03_setup_dcat.sh
+++ ckan/docker-entrypoint.d/03_setup_dcat.sh
@@ -4,4 +4,5 @@
 echo "Loading ckanext-dcat settings in the CKAN config file"
 ckan config-tool $CKAN_INI \
     "ckanext.dcat.base_uri = $CKANEXT__DCAT__BASE_URI" \
+    "ckanext.dcat.catalog_endpoint = $CKANEXT__DCAT__DEFAULT_CATALOG_ENDPOINT" \
     "ckanext.dcat.rdf.profiles = $CKANEXT__DCAT__RDF_PROFILES"
