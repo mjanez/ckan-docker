05edcca600a64c8a0fb269c1d1dd034c8808fbec
mjanez <96422458+mjanez@users.noreply.github.com>
2024-09-20 14:41:15 +0200

    Fix CKAN xloader API token creation
    - Using CKAN_SYSADMIN_NAME envvar

--- ckan/docker-entrypoint.d/02_setup_xloader.sh
+++ ckan/docker-entrypoint.d/02_setup_xloader.sh
@@ -4,7 +4,7 @@
 TOKEN_NAME="xloader"
 
 # Get the list of tokens and extract the IDs for tokens with the specified name
-TOKEN_IDS=$(ckan -c $CKAN_INI user token list ckan_admin | grep "$TOKEN_NAME" | awk -F'[][]' '{print $2}' | tr -d '[]')
+TOKEN_IDS=$(ckan -c $CKAN_INI user token list $CKAN_SYSADMIN_NAME | grep "$TOKEN_NAME" | awk -F'[][]' '{print $2}' | tr -d '[]')
 
 # Revoke each previous token of xloader
 for TOKEN_ID in $TOKEN_IDS
@@ -27,4 +27,4 @@ ckan config-tool $CKAN_INI \
 
 # Create ckanext-xloader API_TOKEN
 echo "[docker-entrypoint.01_setup_xloader] Set up ckanext.xloader.api_token in the CKAN config file"
-ckan config-tool $CKAN_INI "ckanext.xloader.api_token=$(ckan -c $CKAN_INI user token add ckan_admin xloader | tail -n 1 | tr -d '\t')"
+ckan config-tool $CKAN_INI "ckanext.xloader.api_token=$(ckan -c $CKAN_INI user token add $CKAN_SYSADMIN_NAME xloader | tail -n 1 | tr -d '\t')"
